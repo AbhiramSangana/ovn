f079d3c671c0e9ab97464170cc26e3c0b49b0a5e
Abhiram Sangana <sangana.abhiram@nutanix.com>
2022-08-25 15:59:58 +0100

    Fix reg check error

--- controller/physical.c
+++ controller/physical.c
@@ -876,7 +876,7 @@ put_local_common_flows(uint32_t dp_key,
         ofpbuf_clear(ofpacts_p);
         match_set_metadata(&match, htonll(dp_key));
         for (int i = 0; i < MFF_N_LOG_REGS; i++) {
-            if (i != MFF_LOG_ACL_DROP_ZONE) {
+            if ((MFF_REG0 + i) != MFF_LOG_ACL_DROP_ZONE) {
                 put_load(0, MFF_REG0 + i, 0, 32, ofpacts_p);
             }
         }
